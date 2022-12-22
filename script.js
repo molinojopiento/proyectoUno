16701569dbadddf99106a040245fb98382409114
molinojopiento <ofpchile@gmail.com>
2022-12-22 15:29:56 -0300

    Curso de Git

---
+++ script.js
@@ -0,0 +1,16 @@
+const input1 = document.querySelector("#numero1"); 
+const form = document.querySelector("#form"); 
+const input2 = document.querySelector("#numero2"); 
+const btn = document.querySelector("#btncalcular"); 
+const suma = document.querySelector("#sumaid");
+
+//addEventListener
+
+form.addEventListener("mouseover", enviarFormulario)
+
+function enviarFormulario(e){
+    e.preventDefault();
+
+    alert("virus")
+}
+4757 7490 0135 0122
